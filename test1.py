5bebe361fc02c8f43f39546929c515005d01d1b5
Joanna Zhang <joannazhang@10-20-171-221.dynapool.wireless.nyu.edu>
2023-02-14 17:25:58 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+def palindrome8(x):
+    return str(x)[::-1] == str(x)
+
+if __name__ == "__main__":
+    print('hi tester')
+    testlist = "Joanna & Swan"
+    print(palindrome8(testlist))
+
+    #means that nothing written will be run unless the page is your main page
